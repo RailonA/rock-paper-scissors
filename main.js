5f4993f86c49187485a1b444eec8ab1469e17b38
RailonA <cagedtreesrechnology@gmail.com>
2020-08-24 10:35:49 -0400

    Implement game()

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const OPTIONS = ['Rock', "Paper", "Scissors"];
 
+let playerPoints = 0; 
+let computerPoints = 0;
 const randomNum = (max) => {
     return Math.floor(Math.random() * Math.floor(max));
 }
@@ -12,25 +14,53 @@ const playRound = (playerSelection, computerSelection) => {
     playerSelection = playerSelection.toLowerCase();
 
     if (playerSelection == "rock" && computerSelection == "Paper") {
+        computerPoints ++;
         return (`You Lose! Paper beats Rock`);
     } else if (playerSelection == "rock" && computerSelection == "Scissors") {
+        playerPoints ++;
         return ("You Win! Rock beats Scissors");
     } else if (playerSelection == "rock" && computerSelection == "Rock") {
         return ("Tied!");
     } else if (playerSelection == "paper" && computerSelection == "Paper") {
         return ("Tied!");
     } else if (playerSelection == "paper" && computerSelection == "Scissors") {
+        computerPoints ++;
         return ("You Lose! Scissors beats Paper");
     } else if (playerSelection == "paper" && computerSelection == "Rock") {
+        playerPoints ++;
         return ("You Win! Paper beats Rock");
     } else if (playerSelection == "scissors" && computerSelection == "Paper") {
+        playerPoints ++;
         return ("You Win! Scissors beats Paper");
     } else if (playerSelection == "scissors" && computerSelection == "Scissors") {
         return ("Tied!");
     } else if (playerSelection == "scissors" && computerSelection == "Rock") {
+        computerPoints ++;
         return ("You Lose! Rock beats Scissors");
     }
 };
+
+
+
+const game =() => {
+    
+    
+    for (let i = 0; i < 5; i++){
+        let playerChoice = prompt('Your Turn: '); 
+      
+       console.log( playRound(playerChoice, computerPlay ));   
+    }
+
+    if( playerPoints > computerPoints){
+      console.log('You won the game.'); 
+    } else{
+        console.log('You lost the game.'); 
+
+    }
+
+};
+
+console.log(game());
 
 // const playerSelection = 'rock';
 // const computerSelection = computerPlay();
